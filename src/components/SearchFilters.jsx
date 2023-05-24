f0565c52d5182dca9c1f9dda88c9d669302294e6
RukshanNuwan <shanwijendra.dev@gmail.com>
2023-05-24 22:44:35 +0530

    implementing search filter

---
+++ src/components/SearchFilters.jsx
@@ -0,0 +1,19 @@
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {Box} from "@chakra-ui/react";
+
+const SearchFilters = () => {
+  const [] = useState()
+
+  const router = useRouter()
+
+  useEffect(() => {}, [])
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default SearchFilters;
